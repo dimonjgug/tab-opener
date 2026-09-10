f3c77a6c0c52d3753cc49f4691cd8b7b14b35223
dimonjgug <dimqlh@mail.ru>
2026-09-10 14:54:24 +0300

    Обновить run.bat - автопоиск Chrome в разных местах установки

--- run.bat
+++ run.bat
@@ -1,2 +1,21 @@
 @echo off
-chrome.exe --disable-popup-blocking https://raw.githubusercontent.com/dimonjgug/tab-opener/main/index.html
+setlocal enabledelayedexpansion
+
+REM Ищем Chrome в стандартных местах
+set "chromePath="
+
+if exist "C:\Program Files\Google\Chrome\Application\chrome.exe" (
+    set "chromePath=C:\Program Files\Google\Chrome\Application\chrome.exe"
+) else if exist "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" (
+    set "chromePath=C:\Program Files (x86)\Google\Chrome\Application\chrome.exe"
+) else if exist "%LocalAppData%\Google\Chrome\Application\chrome.exe" (
+    set "chromePath=%LocalAppData%\Google\Chrome\Application\chrome.exe"
+)
+
+if "!chromePath!"=="" (
+    echo Chrome не найден! Пожалуйста, установите Google Chrome.
+    pause
+    exit /b 1
+)
+
+start "" "!chromePath!" --disable-popup-blocking "https://raw.githubusercontent.com/dimonjgug/tab-opener/main/index.html"
